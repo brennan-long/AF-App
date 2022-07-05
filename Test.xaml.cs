29ebb06e79c5960c42128391b1be222bf6514e29
Brandon-Boone <108550829+Brandon-Boone@users.noreply.github.com>
2022-07-05 12:27:03 -0400

    Add files via upload

---
+++ Test.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class Test : ContentPage
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://www.bmwusa.com/vehicles/m-models/m5-sedan/overview.html");
+        }
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            // Launch the specified URL in the system browser.
+            await Launcher.OpenAsync("https://www.kennesaw.edu/");
+        }
+    }
+}
